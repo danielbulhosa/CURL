Correct rgb hue extraction, solve NaN backprop issue
In the formula for calculating HSV values from RGB values we need to
divide by a quantity delta that can be zero. When it is zero the output
hue is defined to be zero. This was not captured in the formula from the
original CURL to compute the hue. Instead, it was caught later by
filtering for NaNs and setting them to zero with `img[(img !=
img).detach()] = 0`. This statement however is obscure, did not solve
the NaN issue in our version of the implementation, and is not
compatible with Core ML. We used slicing instead to first identify where
delta is zero and then only divide by its non-zero values. We store the
reciprocals of the non-zero values in a new tensor, and for the indices
for which delta is zero we set the same indices in the new tensor to
zero by default. This approach avoids gradient errors while still
maintaining Core ML compatibility and being clearer in intent.

util.py
@@ -320,22 +320,27 @@ class ImageProcessing(object):
         # Each channel is shape (b, x, y) tensor
         r, g, b = img[:, 0, :, :], img[:, 1, :, :], img[:, 2, :, :]
         
-        # New channel 0, hue
-        img[:, 0, :, :] = (((g-b)/df)*r.eq(mx).float() + (2.0+(b-r)/df)
-                           * g.eq(mx).float() + (4.0+(r-g)/df)*b.eq(mx).float())
+        # New channel 0, hue (see: https://www.rapidtables.com/convert/color/rgb-to-hsv.html)
+        df_inv = ImageProcessing.non_nan_inv(df)        
+        img[:, 0, :, :] = torch.where(df == zero, 
+                                      zero,
+                                      ((g-b)*df_inv)*r.eq(mx).float() + (2.0+(b-r)*df_inv)
+                                      * g.eq(mx).float() + (4.0+(r-g)*df_inv)*b.eq(mx).float())
         img[:, 0, :, :] = img[:, 0, :, :]*60.0
 
+        # Convert hue to range 0 to 360
         img[:, 0, :, :] = img[:, 0, :, :].lt(zero).float(
         )*(img[:, 0, :, :]+360) + img[:, 0, :, :].ge(zero).float()*(img[:, 0, :, :])
 
         img[:, 0, :, :] = img[:, 0, :, :]/360
 
-
         # Set saturation and value, remaining channels
-        img[:, 1, :, :] = mx.ne(zero).float()*(df/mx) + \
-            mx.eq(zero).float()*(zero)
+        mx_inv = ImageProcessing.non_nan_inv(mx)
+        img[:, 1, :, :] = torch.where(mx == zero,
+                                      zero,
+                                      mx.ne(zero).float()*(df*mx_inv) + mx.eq(zero).float()*(zero))
         img[:, 2, :, :] = mx
-        
+
         img = torch.clamp(img, 10**(-9), 1.0)
 
         return img
@@ -519,3 +524,10 @@ class ImageProcessing(object):
         img = img.contiguous()
 
         return img, slope_sqr_diff
+
+    @staticmethod
+    def non_nan_inv(tensor):
+        tensor_inv = torch.zeros(tensor.shape, device=torch.device('cuda'), dtype=torch.float)
+        tensor_inv[tensor != 0] = 1/(tensor[tensor != 0])
+        
+        return tensor_inv