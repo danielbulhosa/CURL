febd5f90a5583cd37f977ba6667fd1158c88037e
danielbulhosa <danielbulhosa@squareup.com>
2021-11-25 02:49:58 +0000

    Clarify logging, remove test evaluation from train script

--- main.py
+++ main.py
@@ -150,20 +150,15 @@ def main():
         
         training_ids = data.get_data_ids(training_img_dirpath+"/images_train.txt")
         valid_ids = data.get_data_ids(training_img_dirpath+"/images_valid.txt")
-        test_ids = data.get_data_ids(training_img_dirpath+"/images_test.txt")
         
         training_data_dict = data.filter_data_dict(data_dict, training_ids)
         validation_data_dict = data.filter_data_dict(data_dict, valid_ids)
-        testing_data_dict = data.filter_data_dict(data_dict, test_ids)
         
         training_dataset = data.Dataset(data_dict=training_data_dict, normaliser=1, is_valid=False)
         validation_dataset = data.Dataset(data_dict=validation_data_dict, normaliser=1, is_valid=True)
-        testing_dataset = data.Dataset(data_dict=testing_data_dict, normaliser=1,is_valid=True)
 
         training_data_loader = torch.utils.data.DataLoader(training_dataset, batch_size=batch_size, shuffle=True,
                                                            pin_memory=True, num_workers=6)
-        testing_data_loader = torch.utils.data.DataLoader(testing_dataset, batch_size=batch_size, shuffle=False,
-                                                          pin_memory=True, num_workers=6)
         validation_data_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=batch_size, shuffle=False,
                                                              pin_memory=True, num_workers=6)
    
@@ -171,23 +166,13 @@ def main():
         net.cuda()
 
         logging.info('######### Network created #########')
-        logging.info('Architecture:\n' + str(net))
-
-        for name, param in net.named_parameters():
-            if param.requires_grad:
-                print(name)
 
         criterion = model.CURLLoss(ssim_window_size=5)
 
         '''
-        The following objects allow for evaluation of a model on the testing and validation splits of a dataset
+        The following objects allow for evaluation of a model on the validation split of the dataset
         '''
-        validation_evaluator = metric.Evaluator(
-            criterion, validation_data_loader, "valid", log_dirpath)
-        testing_evaluator = metric.Evaluator(
-            criterion, testing_data_loader, "test", log_dirpath)
-
-    
+        validation_evaluator = metric.Evaluator(criterion, validation_data_loader, "valid", log_dirpath)
         start_epoch=0
 
         if (checkpoint_filepath is not None) and (inference_img_dirpath is None):
@@ -214,6 +199,7 @@ def main():
         net.train()
 
         for epoch in range(start_epoch,num_epoch):
+            logging.info("######### Epoch {}: Train #########".format(epoch + 1))
 
             # train loss
             examples = 0.0
@@ -240,7 +226,7 @@ def main():
                 examples += batch_size
                 
                 writer.add_scalar('Loss/train', loss_scalar, examples)
-                batch_pbar.set_description('Train Loss: {}'.format(loss_scalar))
+                batch_pbar.set_description('Epoch {}. Train Loss: {}'.format(epoch, loss_scalar))
 
 
             logging.info('[%d] train loss: %.15f' %
@@ -250,29 +236,23 @@ def main():
             # Valid loss
             if (epoch + 1) % valid_every == 0:
 
-                logging.info("Evaluating model on validation dataset")
+                logging.info("######### Epoch {}: Validation #########".format(epoch + 1))
 
-                valid_loss, valid_psnr, valid_ssim = validation_evaluator.evaluate(
-                    net, epoch)
-                test_loss, test_psnr, test_ssim = testing_evaluator.evaluate(
-                    net, epoch)
+                valid_loss, valid_psnr, valid_ssim = validation_evaluator.evaluate(net, epoch)
 
                 # update best validation set psnr
                 if valid_psnr > best_valid_psnr:
 
                     logging.info(
                         "Validation PSNR has increased. Saving the more accurate model to file: " + 
-                        'curl_validpsnr_{}_validloss_{}_testpsnr_{}_testloss_{}_epoch_{}_model.pt'.format(valid_psnr, valid_loss, test_psnr, 
-                                                                                                          test_loss, epoch))
+                        'curl_validpsnr_{}_validloss_{}_epoch_{}_model.pt'.format(valid_psnr, valid_loss, epoch))
 
                     best_valid_psnr = valid_psnr
                     snapshot_prefix = os.path.join(
                         log_dirpath, 'curl')
-                    snapshot_path = snapshot_prefix + '_validpsnr_{}_validloss_{}_testpsnr_{}_testloss_{}_epoch_{}_model.pt'.format(valid_psnr,
-                                                                                                                                    valid_loss,
-                                                                                                                                    test_psnr, 
-                                                                                                                                    test_loss,
-                                                                                                                                    epoch + 1)
+                    snapshot_path = snapshot_prefix + '_validpsnr_{}_validloss_{}_epoch_{}_model.pt'.format(valid_psnr,
+                                                                                                            valid_loss,
+                                                                                                            epoch + 1)
                     '''
                     torch.save(net, snapshot_path)
                     '''
@@ -285,31 +265,6 @@ def main():
                          }, snapshot_path)
 
                 net.train()
-
-        '''
-        Run the network over the testing dataset split
-        '''
-        snapshot_prefix = os.path.join(
-                        log_dirpath, 'curl')
-
-        valid_loss, valid_psnr, valid_ssim = validation_evaluator.evaluate(
-                    net, epoch)
-        test_loss, test_psnr, test_ssim = testing_evaluator.evaluate(
-                    net, epoch)
-
-        snapshot_path = snapshot_prefix + '_validpsnr_{}_validloss_{}_testpsnr_{}_testloss_{}_epoch_{}_model.pt'.format(valid_psnr,
-                                                                                                                                    valid_loss.tolist()[
-                                                                                                                                        0],
-                                                                                                                                    test_psnr, test_loss.tolist()[
-                                                                                                                                        0],
-                                                                                                                                    epoch +1)
-        snapshot_prefix = os.path.join(log_dirpath, 'curl')
-        torch.save({
-                        'epoch': epoch+1,
-                         'model_state_dict': net.state_dict(),
-                        'optimizer_state_dict': optimizer.state_dict(),
-                         'loss': loss,
-                         }, snapshot_path)
 
 if __name__ == "__main__":
     main()
